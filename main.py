0e274fee3054fab6deb841a510377d201d9f580f
josewtf01 <soulsilversx@gmail.com>
2021-03-08 03:27:26 -0600

    Se definieron variables de cantidad minima de goles, equipos

--- main.py
+++ main.py
@@ -4,26 +4,37 @@ def ReadJson(jsonObject):
     niveles = {"A":5,"B":10,"C":15,"Cuauh":20}
     print(niveles)
     print("\n")
-
+    
     #Convetir un objecto json en un diccionario de python
     y = json.loads(jsonObject)
+
+    #Definir la variable de la cantidad de jugadores
+    numberOfPlayers = len(y["jugadores"])
+
+    #Crear un conjuto de diferentes equipos
+    equipos = set()
+
+    #definir la cantidad minima de goles por equipo
+    MinimumGoalsPerTeam = 0
     
+    #Imprimir los nombres de cada jugador
+    for i in range(numberOfPlayers):
+        print(y["jugadores"][i]["nombre"], ": nivel ",y["jugadores"][i]["nivel"])
+        nivel  = y["jugadores"][i]["nivel"]
+        equipos.add(y["jugadores"][i]["equipo"])
+        MinimumGoalsPerTeam += niveles[nivel]
+    print("goles minimos por equipo: ",MinimumGoalsPerTeam)
+    print("\n")
+    print("equipos",equipos)
+
+    print(y["jugadores"][0])
+    print(type(y["jugadores"][0]),"\n")
+
     #Imprimir el tipo de la clave jugadores
     #print(y)
     print(y["jugadores"])
     print(type(y["jugadores"]))
     print("\n")
-
-    #Definir la variable de la cantidad de jugadores
-    numberOfPlayers = len(y["jugadores"])
-    #Imprimir los nombres de cada jugador
-    for i in range(numberOfPlayers):
-        print(y["jugadores"][i]["nombre"])
-    print("\n")
-
-    print(y["jugadores"][0])
-    print(type(y["jugadores"][0]))
-
 
 x = """{
    "jugadores" : [  
